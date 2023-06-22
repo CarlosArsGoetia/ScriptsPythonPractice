46b391ca6d2bad948d8b3674f91092cdd64cd7d7
CarHerJR <74396964+CarlosArsGoetia@users.noreply.github.com>
2023-06-22 08:41:37 +0000

    Create telephoneCarrier.py
    this simple python script detects the service provider for a phone number
     using the 
    "phonenumbers" library

---
+++ telephoneCarrier.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""you can install phonenumbers like this 
+ pip install phonenumbers"""
+from colorama import Fore
+import phonenumbers 
+from phonenumbers import geocoder, timezone, carrier
+import sys, signal
+
+def def_handler(sig,frame):
+    print(Fore.WHITE+"\n\n[***] Running out...[***]\n")
+    sys.exit(1)
+
+#Ctrl+C for exit
+signal.signal(signal.SIGINT, def_handler)
+
+
+while True:
+     
+        target = input(Fore.LIGHTBLACK_EX+"Please enter your phone number" + Fore.LIGHTYELLOW_EX+"(PhoneNumberFormat)" + Fore.YELLOW+"\nEXAMPLE: +58412330xxx\n" + Fore.LIGHTBLACK_EX+" Type the Number: ")
+        if target.isalpha() is True:
+            print("Enter a valid phone number")
+        else: break   
+
+
+number = phonenumbers.parse(target)
+
+zone = timezone.time_zones_for_number(number)
+
+geo = geocoder.description_for_number(number, 'en')
+
+carrier = carrier.name_for_number(number, 'en')
+
+print(Fore.RED+f"\nZone: {zone}  \nCountry: {geo}  \nCompany: {carrier}")
