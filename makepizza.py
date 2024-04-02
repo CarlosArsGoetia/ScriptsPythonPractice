83db316d6f33b8b48d740df68ac0a1613445d086
CarHerJR <74396964+CarlosArsGoetia@users.noreply.github.com>
2024-04-02 12:14:48 +0000

    Update makepizza.py

--- makepizza.py
+++ makepizza.py
@@ -8,13 +8,20 @@ def def_handler(sig,frame):
 #Para una salida limpia
 signal.signal(signal.SIGINT, def_handler)
 
-#Empezamos
+def imprimir_receta_pizza():
 
-welcome = "Bienvenido a un Script Basico para calcular la noche de Pizzas\n La receta va de mi parte :D "
-print(f"Hola // {welcome}")
+    print(f"\nTu receta para hacer un paston de masa para pizza que pesara: {weight}gr ")
+    print(f"[+]Harina Panadera: {f_grams.__round__():.0f}gr")
+    print(f"[+]Agua: {w_grams.__round__():.0f}gr")
+    print(f"[+]Levadura: {y_grams.__round__():.0f}gr [!] Si es levadura instantanea usa la mitad.")
+    print(f"[+]Sal: {s_grams.__round__():.0f}gr")
+    print(f"[+]Aceite: {o_grams.__round__():.0f}gr")
+    print(f"\n[!]Despues del correcto amasado---> Deja la masa en reposo 30min (Cubierta)\n")
+
+#Empezamos
+print(f"Script para pizzas \n La receta va de mi parte :D ")
 
 #receta representada en porcentajes
-
 harina = 100
 agua = 57
 levadura = 2
@@ -24,7 +31,7 @@ aceite = 15
 #Suma de todos los ingredientes para calcular el Factor Panadero(bf)
 total_ingredients = harina + agua + aceite + levadura + sal
 
-#Tambien necesitamos preguntar por el peso del paston
+#Tambien necesitamos preguntar por el peso del paston 
 while True:
     print(f"[+]TIP--el peso una de Pizza pequeña promedio oscila entre 200 a 300 gramos\n")
     weight  = input("[+]¿Cuantos gramos quieres que pese el paston (masa total)?:  ")
@@ -35,27 +42,18 @@ while True:
         print("[!] Ingrese el valor en gramos \n Ejemplo 1000 para un kilo | 1500  seria un Kilo y medio")
 
 
-
-#Con el peso y la suma total de porcetajes de los ingredientes
+#Con el peso y la suma total del porcetaje de ingredientes  podemos calcular el factor panadero (bf)
 bf = weight  / total_ingredients
 
 #Por cortesia mostramos el Factor Panadero
 print(f"\nTu factor panadero es de: {bf:.2f}")
-#trasparemos la receta de 'porcentaje' a 'gramos'
+
+#trasparemos la receta de 'porcentaje' a 'gramos'  bf * ingrediente
 f_grams = bf * harina
 w_grams = bf * agua
 y_grams = bf * levadura
 s_grams = bf * sal  
 o_grams = bf * aceite
 
-#Mostramosla receta que varia en proporciones dependiendo del tamaño del paston
-
-print(f"\nTu receta para hacer un paston de masa para pizza que pesara: {weight}gr ")
-print(f"[+]Harina Panadera: {f_grams.__round__():.0f}gr")
-print(f"[+]Agua: {w_grams.__round__():.0f}gr")
-print(f"[+]Levadura: {y_grams.__round__():.0f}gr")
-print(f"[+]Sal: {s_grams.__round__():.0f}gr")
-print(f"[+]Aceite: {o_grams.__round__():.0f}gr")
-print(f"\n[!]Despues del correcto amasado---> Deja la masa en reposo 30min (Cubierta)\nBuen Provecho-")
-
-
+#Mostramosla receta que varia en proporciones dependiendo del tamaño del paston(peso total de la masa)
+imprimir_receta_pizza()
